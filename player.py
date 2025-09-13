1954f711ba615d664e03b395f03b66952d57027d
NoSpaceWhy <ashrayvarughese@gmail.com>
2025-09-13 21:47:28 +0530

    sooo i made it so that the rect is dislay yay.... idk how tostop a player from otu of the screen

--- player.py
+++ player.py
@@ -8,10 +8,15 @@ class Player:
         self.color = (255, 0, 0)  # Red color
         self.size = 100  # Size of the player square
         self.speed = 5  # Movement speed
-        
+        self.player_rect = pygame.Rect(self.x, self.y, self.size, self.size)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size))
+        self.player_rect.topleft = self.x, self.y
+
+        pygame.draw.rect(screen, self.color,self.player_rect)
+
+        # if self.player_rect.bottomright == screen.get_width():
+        #     self.x = screen.get_width() - 20
 
     def move(self, dx, dy):
         self.x += dx
@@ -26,16 +31,15 @@ class Player:
             self.move(0, -1 * self.speed)
         if keys[pygame.K_DOWN]:
             self.move(0, self.speed)
-
+        
+        # border collision
+        
     def action(self, keys):
         if keys[pygame.K_SPACE]:
             self.speed = self.speed + 5
             print(self.speed)
         else:
             self.speed = 5
-
-    def player_camera(self, screen, keys):
-        pass
 
     def player_run(self, screen, keys):
         # draw the player
