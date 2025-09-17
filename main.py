cc3e346c8f0e70e8a249d1ec6ea78a41d1e5029d
NoSpaceWhy <ashrayvarughese@gmail.com>
2025-09-17 21:48:37 +0530

    a new start cause i am brain fried

--- main.py
+++ main.py
@@ -1,60 +1,24 @@
+import sys
 import pygame
-import pytmx
 
-import player
-import enemy
+# my files
 
 pygame.init()
 
-# screen dimensions and setup
-screen_width = 800
-screen_height = 700
-screen = pygame.display.set_mode((screen_width, screen_height))
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((800,600))
+        pygame.display.set_caption("rouge like ")
+        self.clock = pygame.time.Clock()
 
-pygame.display.set_caption("RougeLike") #window title
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-clock = pygame.time.Clock() # fps controller
-
-# map
-scale = 2
-tmx_data = pytmx.util_pygame.load_pygame("maps/map.tmx")
-map_width = tmx_data.width
-map_height = tmx_data.height
-
-tile_width = tmx_data.tilewidth * scale
-tile_height = tmx_data.tileheight * scale
-
-map_width = map_width * tile_width
-map_height = map_height * tile_height
-
-# player
-p = player.Player(400, 300)
-e = enemy.Enemy()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
-    screen.fill((0, 0, 0))
-
-    keys = pygame.key.get_pressed()
-
-
-    # map code
-    for layer in tmx_data.visible_layers:
-        if isinstance(layer, pytmx.TiledTileLayer):
-            for x, y, gid in layer:
-                tile = tmx_data.get_tile_image_by_gid(gid)
-                if tile:
-                    screen.blit(tile, (x * tile_width, y * tile_height))
-
-    e.run(screen)
-    e.move()
-
-    p.player_run(screen, keys)
-
-
-    clock.tick(60)
-    pygame.display.flip()
+            
+            pygame.display.update()
+            self.clock.tick(60)
+Game().run()
--- maps/map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="map" tilewidth="16" tileheight="16" tilecount="180" columns="20">
- <image source="326.001.png" width="320" height="146"/>
-</tileset>
